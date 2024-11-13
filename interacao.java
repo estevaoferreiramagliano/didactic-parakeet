328901e90c50a48253af1c5e81a0a64f420ca1a4
estevaoferreiramagliano <106787816+estevaoferreiramagliano@users.noreply.github.com>
2024-11-13 14:40:18 -0300

    interacao.java

---
+++ interacao.java
@@ -0,0 +1,3 @@
+// Função para pesquisa (apenas um exemplo simples)  
+
+function searchFunction() { const query = document.getElementById('searchInput').value; if(query) { alert('Você está buscando por: ' + query); } else { alert('Digite algo na barra de pesquisa!'); } } 
