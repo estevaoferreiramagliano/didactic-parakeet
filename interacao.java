2ddde455f2e1e5e769e68b064330c35be21745ad
estevaoferreiramagliano <106787816+estevaoferreiramagliano@users.noreply.github.com>
2024-11-14 14:12:08 -0300

    interacao.java

--- interacao.java
+++ interacao.java
@@ -1,3 +1,13 @@
-// Função para pesquisa (apenas um exemplo simples)  
-
-function searchFunction() { const query = document.getElementById('searchInput').value; if(query) { alert('Você está buscando por: ' + query); } else { alert('Digite algo na barra de pesquisa!'); } } 
+// Função para realizar a pesquisa
+function searchFunction() {
+  // Obter o valor digitado na barra de pesquisa
+  const query = document.getElementById('searchInput').value;
+  // Verificar se o campo de pesquisa está vazio ou não
+  if (query) {
+    // Exibir o termo de pesquisa digitado
+    alert('Você está buscando por: ' + query);
+  } else {
+    // Solicitar ao usuário para digitar algo
+    alert('Digite algo na barra de pesquisa!');
+  }
+}
